9c1a2e25b8d180a3a19eaf8e56ace9ed27744b81
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-03-25 22:03:35 +0300

    base functional

--- clc.li.py
+++ clc.li.py
@@ -1,16 +1,20 @@
-import requests
-
+import os
 from urllib.parse import urlparse
+
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 API_URL = 'https://clc.li/api/url/add'
 LIST_LINK_API_URL = 'https://clc.li/api/urls?short='
+TOKEN = os.environ['TOKEN']
 
-token='api_key_tut'
 
-#url = 'https://ya.ru'
-#url = input()
-link = 'clc.li/tYGho'
+url = input()
+
 
 def shorten_link(token, url):
     payload = {
@@ -26,7 +30,6 @@ def shorten_link(token, url):
         responce.status_code
     except requests.exceptions.HTTPError:
         return f'Ошибка сокрашения: {url}'
-    #print(responce.raise_for_status)
     try:
         full_url = responce.json()['shorturl']
     except KeyError:
@@ -41,24 +44,34 @@ def count_clics(token, link):
         'Authorization': 'Bearer ' + token,
         'Content-Type': 'application/json'
     }
-    #try:
-    responce = requests.get(LIST_LINK_API_URL + link, headers=headers)
-    print(responce.status_code)
-    print(responce.text)
-    '''
-    except requests.exceptions.HTTPError:
-        return f'Ошибка сокрашения: {url}'
-    #print(responce.raise_for_status)
     try:
-        full_url = responce.json()['shorturl']
+        responce = requests.get(LIST_LINK_API_URL + url, headers=headers)
+        responce.status_code
+    except requests.exceptions.HTTPError:
+        return f'Ошибка получения данных: {link}'
+    try:
+        clicks = responce.json()['data']['clicks']
     except KeyError:
-        raise SystemExit(f'Ошибка! Ввели неправильный URL - {url} ')
-    parsed = urlparse(full_url)
-    return f"{parsed.netloc}{parsed.path}"
-    '''
+        raise SystemExit('Неверная короткая ссылка!')
+    return clicks
 
-#shorten_link(token, url)
 
-#print('Короткая ссылка', shorten_link(token, url))
-print('Чтототам', count_clics(token, link))
+def is_bitlink(url):
+    headers = {
+        'User-Agent': 'curl',
+        'Authorization': 'Bearer ' + TOKEN,
+        'Content-Type': 'application/json'
+    }
+    try:
+        responce = requests.get(LIST_LINK_API_URL + url, headers=headers)
+        if responce.json()['error'] == 0:
+            return True
+    except requests.exceptions.HTTPError:
+        return False
 
+
+if __name__ == '__main__':    
+    if is_bitlink(url):
+        print('Количество кликов:', count_clics(TOKEN, url))  
+    else:
+        print('Короткая ссылка', shorten_link(TOKEN, url))

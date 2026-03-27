aa3972eaabe7b8c62bb0d318b030d4b6207b0fc7
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-03-27 14:29:40 +0300

    fix for review

--- clc.li.py
+++ clc.li.py
@@ -5,73 +5,57 @@ import requests
 from dotenv import load_dotenv
 
 
-load_dotenv()
-
-
 API_URL = 'https://clc.li/api/url/add'
-LIST_LINK_API_URL = 'https://clc.li/api/urls?short='
-TOKEN = os.environ['TOKEN']
-
-
-url = input()
 
 
 def shorten_link(token, url):
     payload = {
         'url': url,
-    }
-    headers = {
-        'User-Agent': 'curl',
-        'Authorization': 'Bearer ' + token,
-        'Content-Type': 'application/json'
-    }
-    try:
-        response = requests.post(API_URL, headers=headers, json=payload)
-        response.status_code
-    except requests.exceptions.HTTPError:
-        return f'Ошибка сокрашения: {url}'
-    try:
-        full_url = response.json()['shorturl']
-    except KeyError:
-        raise SystemExit(f'Ошибка! Ввели неправильный URL - {url} ')
-    parsed = urlparse(full_url)
+    }   
+    response = requests.post(API_URL, headers=headers, json=payload)
+    response.raise_for_status()
+    response = response.json()
+    if 'shorturl' not in response:
+        raise ValueError(f"API не вернул короткую ссылку для {url}")
+    parsed = urlparse(response['shorturl'])    
     return f"{parsed.netloc}{parsed.path}"
 
 
 def count_clics(token, link):
-    headers = {
-        'User-Agent': 'curl',
-        'Authorization': 'Bearer ' + token,
-        'Content-Type': 'application/json'
-    }
-    try:
-        response = requests.get(LIST_LINK_API_URL + url, headers=headers)
-        response.status_code
-    except requests.exceptions.HTTPError:
-        return f'Ошибка получения данных: {link}'
-    try:
-        clicks = response.json()['data']['clicks']
-    except KeyError:
-        raise SystemExit('Неверная короткая ссылка!')
+    response = requests.get(list_link_api_url, headers=headers)
+    response.status_code
+    clicks = response.json()['data']['clicks']
     return clicks
 
 
 def is_bitlink(url):
-    headers = {
-        'User-Agent': 'curl',
-        'Authorization': 'Bearer ' + TOKEN,
-        'Content-Type': 'application/json'
-    }
-    try:
-        response = requests.get(LIST_LINK_API_URL + url, headers=headers)
-        if response.json()['error'] == 0:
-            return True
-    except requests.exceptions.HTTPError:
+    response = requests.get(list_link_api_url, headers=headers)
+    response.raise_for_status()
+    if response.json()['error'] == 0:
+        return True
+    else:
         return False
 
 
-if __name__ == '__main__':    
-    if is_bitlink(url):
-        print('Количество кликов:', count_clics(TOKEN, url))  
-    else:
-        print('Короткая ссылка', shorten_link(TOKEN, url))
+if __name__ == '__main__': 
+    load_dotenv()
+
+    token_clcli = os.environ['TOKEN_CLCLI']
+    
+    headers = {
+        'User-Agent': 'curl',
+        'Authorization': f'Bearer {os.environ['TOKEN_CLCLI']}',
+        'Content-Type': 'application/json'
+    }
+    
+    url = input()
+
+    list_link_api_url = f'https://clc.li/api/urls?short={url}'
+    
+    try:
+        if is_bitlink(url):
+            print('Количество кликов:', count_clics(token_clcli, url))  
+        else:
+            print('Короткая ссылка', shorten_link(token_clcli, url))
+    except (requests.exceptions.HTTPError, KeyError):
+        raise SystemExit("Произошла ошибка при работе с API или ответом сервера")
